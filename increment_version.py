10c04e32cf6c0d26e01c8586dfe88e661456b1ca
Karel Břinda <karel.brinda@gmail.com>
2018-10-27 19:28:52 -0400

    Run yapf

--- increment_version.py
+++ increment_version.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
-fn="svg42pdf/version.py"
+fn = "svg42pdf/version.py"
 
 exec(open(fn).read())
 
-numbers=VERSION.split(".")
-numbers[-1]=str(int(numbers[-1])+1)
+numbers = VERSION.split(".")
+numbers[-1] = str(int(numbers[-1]) + 1)
 
-version=".".join(numbers)
+version = ".".join(numbers)
 
-with open(fn,"w") as f:
-	f.write('VERSION="{}"'.format(version))
+with open(fn, "w") as f:
+    f.write('VERSION="{}"'.format(version))
